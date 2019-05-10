46577c6e6ade28dfa3eaf2928e76b175a8920e65
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-05-10 18:41:01 -0400

    Message Form Test

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "you@yourdomain.com"; // Add Your Email
-$sitename = "Osaka - One Page Portfolio Bootstrap 4"; // Add Your Site Name
+$recepient = "richard@richardf.ink"; // Add Your Email
+$sitename = "Richard Fink's Portfolio"; // Add Your Site Name
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
@@ -10,3 +10,5 @@ $message = "Name: $name\nEmail: $email\nMessage: $text";
 
 $pagetitle = "New request from site \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
